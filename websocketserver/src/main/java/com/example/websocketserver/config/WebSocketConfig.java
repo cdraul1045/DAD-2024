17213d099d87ed57ce394e7517a6810361141cb1
brayanraul545 <brayanraulcondori@gmail.com>
2024-10-20 22:19:48 -0500

    intento1

--- websocketserver/src/main/java/com/example/websocketserver/config/WebSocketConfig.java
+++ websocketserver/src/main/java/com/example/websocketserver/config/WebSocketConfig.java
@@ -23,3 +23,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     }
 }
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/tema");
+        registry.setApplicationDestinationPrefixes("/app");
+
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").setAllowedOrigins("*");
+
+    }
+}
